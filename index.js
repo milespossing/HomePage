e8321e4d2d287e90a00f66da92c1fad1fb449ba1
Miles Possing <7330275+milespossing@users.noreply.github.com>
2020-07-04 20:06:53 -0500

    Quick fixes

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const argv = require('yargs').default('ports',[5000,5001]).argv;
+const argv = require('yargs')
+  .default('http',5000)
+  .default('https',5001)
+  .argv;
 const app = require('./src/app.js').build(argv);
 
-let [http,https] = argv.ports;
-
-if (!argv.http){
-    app.setupHttps(https);
+if (!argv.httpOnly){
+    app.setupHttps(argv.https);
 }
 
-app.setupHttp(argv.p || http);
+app.setupHttp(argv.http);
